ecefc941306f2e495bab526820df2c2cf618afdc
Narendra Singh Rajput <rajputnarendra62@gmail.com>
2021-12-07 22:22:19 +0530

    Add MealIem,Summary,Form,Card,Cart module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header/>
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
